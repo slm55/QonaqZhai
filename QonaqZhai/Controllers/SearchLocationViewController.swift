014a46c9d021fdcfd4d9b0baa2590436bba16951
Aslan <shamkhanislam00@gmail.com>
2022-08-15 13:31:23 +0600

    search bar customized

--- QonaqZhai/Controllers/SearchLocationViewController.swift
+++ QonaqZhai/Controllers/SearchLocationViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 class SearchLocationViewController: UIViewController {
     private let searchController: UISearchController = {
         let searchController = UISearchController()
+        searchController.searchBar.showsCancelButton = false
+        let textField = searchController.searchBar.searchTextField
+        textField.backgroundColor = .systemBackground
+        textField.placeholder = "Enter destination"
+        textField.layer.borderWidth = 6
+        textField.layer.cornerRadius = 4
+        textField.layer.borderColor = UIColor.orange.cgColor
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = UIImage(systemName: "chevron.left")
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
+        let button = UIButton()
+        button.configuration = configuration
+        textField.leftView = button
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        textField.centerXAnchor.constraint(equalTo: searchController.searchBar.centerXAnchor).isActive = true
+        textField.widthAnchor.constraint(equalTo: searchController.searchBar.widthAnchor, constant: -32).isActive = true
         return searchController
     }()
     
@@ -21,6 +38,10 @@ class SearchLocationViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationItem.searchController = searchController
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
     }
     
     override func viewDidAppear(_ animated: Bool) {
